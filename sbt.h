77420f38b2500528774eb0bdb7a573d58016a23a
dang0x5f <d4n9xcv@gmail.com>
2025-05-04 21:40:39 -0400

    Added sbt_id and sbt_msg.

--- sbt.h
+++ sbt.h
@@ -2,9 +2,6 @@
 // 
 // - define file / stdout
 //   - if null , default file_output_name
-//
-// - input identifier const char *
-//   - if null , default identifier
 
 #ifndef SBT_H_
 #define SBT_H_
@@ -13,27 +10,34 @@
 #include <sys/time.h>
 
 struct timeval tvp1 , tvp2;
-struct timezone tzp1 , tzp2;
+// struct timezone tzp1 , tzp2;
 
-void start_sbt(void);
+void start_sbt(char *, char *);
 void end_sbt(char *);
 
 #endif // SBT_H
 
 #ifdef SBT_IMPLEMENTATION
 
-void start_sbt(void)
+void start_sbt(char *sbt_id, char *sbt_msg)
 {
-    gettimeofday(&tvp1, &tzp1);
+    if(sbt_msg != NULL)
+        printf("[%s] %s\n", sbt_id, sbt_msg);
+    // gettimeofday(&tvp1, &tzp1);
+    gettimeofday(&tvp1, NULL);
 }
 
 void end_sbt(char *sbt_id)
 {
-    gettimeofday(&tvp2, &tzp2);
+    // gettimeofday(&tvp2, &tzp2);
+    gettimeofday(&tvp2, NULL);
 
     int diff_sec = tvp2.tv_sec - tvp1.tv_sec;
     int diff_ms  = tvp2.tv_usec - tvp1.tv_usec;
-    printf("[%s] %ds %dms\n", sbt_id, diff_sec, diff_ms);
+    if(sbt_id != NULL)
+        printf("[%s] %ds %dms\n", sbt_id, diff_sec, diff_ms);
+    else
+        printf(" > %ds %dms\n", diff_sec, diff_ms);
 }
 
 #endif // SBT_IMPLEMENTATION
--- test.c
+++ test.c
@@ -1,11 +1,23 @@
 #define SBT_IMPLEMENTATION
 #include "sbt.h"
 
+void test01(void)
+{
+    start_sbt(NULL,NULL);
+    end_sbt(NULL);
+}
+
+void test02(void)
+{
+    start_sbt("sbt_id","start");
+    end_sbt("sbt_id");
+}
+
 int main(void)
 {
-    start_sbt();
 
-    end_sbt("sbt_id");
+    test01();
+    test02();
 
     return(0);
 }
