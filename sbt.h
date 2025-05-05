dd641e8579e61bd55a26750d5c7570c34c0ca581
dang0x5f <d4n9xcv@gmail.com>
2025-05-04 22:18:05 -0400

    Add simple and verbose calls

--- sbt.h
+++ sbt.h
@@ -12,22 +12,37 @@
 struct timeval tvp1 , tvp2;
 // struct timezone tzp1 , tzp2;
 
-void start_sbt(char *, char *);
-void end_sbt(char *);
+void start_sbt(void);
+void end_sbt(void);
+void start_sbt_v(char *, char *);
+void end_sbt_v(char *);
 
 #endif // SBT_H
 
 #ifdef SBT_IMPLEMENTATION
 
-void start_sbt(char *sbt_id, char *sbt_msg)
+void start_sbt(void)
+{
+    gettimeofday(&tvp1, NULL);
+}
+
+void end_sbt(void)
+{
+    gettimeofday(&tvp2, NULL);
+    int diff_sec = tvp2.tv_sec - tvp1.tv_sec;
+    int diff_ms  = tvp2.tv_usec - tvp1.tv_usec;
+    printf(" > %ds %dms\n", diff_sec, diff_ms);
+}
+
+void start_sbt_v(char *sbt_id, char *sbt_msg)
 {
     if(sbt_msg != NULL)
-        printf("[%s] %s\n", sbt_id, sbt_msg);
+        printf(" [%s] %s\n", sbt_id, sbt_msg);
     // gettimeofday(&tvp1, &tzp1);
     gettimeofday(&tvp1, NULL);
 }
 
-void end_sbt(char *sbt_id)
+void end_sbt_v(char *sbt_id)
 {
     // gettimeofday(&tvp2, &tzp2);
     gettimeofday(&tvp2, NULL);
@@ -35,7 +50,7 @@ void end_sbt(char *sbt_id)
     int diff_sec = tvp2.tv_sec - tvp1.tv_sec;
     int diff_ms  = tvp2.tv_usec - tvp1.tv_usec;
     if(sbt_id != NULL)
-        printf("[%s] %ds %dms\n", sbt_id, diff_sec, diff_ms);
+        printf(" [%s] %ds %dms\n", sbt_id, diff_sec, diff_ms);
     else
         printf(" > %ds %dms\n", diff_sec, diff_ms);
 }
--- test.c
+++ test.c
@@ -3,21 +3,39 @@
 
 void test01(void)
 {
-    start_sbt(NULL,NULL);
-    end_sbt(NULL);
+    printf("test01\n");
+
+    start_sbt();
+    end_sbt();
+
+    printf("\n");
 }
 
 void test02(void)
 {
-    start_sbt("sbt_id","start");
-    end_sbt("sbt_id");
+    printf("test02\n");
+
+    start_sbt_v(NULL,NULL);
+    end_sbt_v(NULL);
+
+    printf("\n");
+}
+
+void test03(void)
+{
+    printf("test03\n");
+
+    start_sbt_v("sbt_id","start");
+    end_sbt_v("sbt_id");
+
+    printf("\n");
 }
 
 int main(void)
 {
-
     test01();
     test02();
+    test03();
 
     return(0);
 }
